d1c648e68c7eff148e063a35b92e4a6b948b796c
frank gimeno <frankgimeno3@gmail.com>
2023-10-10 23:29:31 +0200

    boton borrar funcional

--- components/ChatRow.tsx
+++ components/ChatRow.tsx
@@ -1,6 +1,6 @@
 import { db } from '@/firebase';
 import { ChatBubbleLeftIcon, TrashIcon } from '@heroicons/react/24/solid';
-import { collection, orderBy, query } from 'firebase/firestore';
+import { collection, deleteDoc, doc, orderBy, query } from 'firebase/firestore';
 import { useSession } from 'next-auth/react';
 import Link from 'next/link';
 import { usePathname, useRouter } from 'next/navigation';
@@ -25,6 +25,11 @@ function ChatRow({ id }: Props) {
         setActive(pathname.includes(id)) 
     }, [pathname])
 
+
+    const removeChat = async( ) =>{
+        await deleteDoc(doc(db, session?.user?.email!, 'chats', id))
+        router.replace("/")
+    }
     return (
         <div>
             <Link href={`/chat/${id}`} className={`chatRow justify-center ${active && "bg-gray-700/50"}`}>
@@ -33,6 +38,7 @@ function ChatRow({ id }: Props) {
                     {messages?.docs[messages?.docs.length - 1]?.data().text || "New Chat"}
                 </p>
                 <TrashIcon
+                onClick={removeChat}
                     className='h-5 w-5 text-gray-700 hover:text-red-700' />
             </Link>
         </div>
