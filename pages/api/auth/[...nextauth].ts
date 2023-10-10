85f857d22edacda67560d18079c632a49c479c8f
frank gimeno <frankgimeno3@gmail.com>
2023-10-10 17:12:35 +0200

    soluciono error google

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -8,7 +8,6 @@ export const authOptions = {
       clientId: process.env.GOOGLE_ID!,
       clientSecret: process.env.GOOGLE_ID_SECRET! ,
     }),
-    // ...add more providers here
-  ],
+   ],
 }
 export default NextAuth(authOptions)
