92cfc11509ef4c05d928b023b171eb750e42c755
frank gimeno <frankgimeno3@gmail.com>
2023-10-10 18:00:33 +0200

    muestra imagen en barra

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,6 +1,10 @@
+"use client"
 import NewChat from "./NewChat" 
+import {useSession} from "next-auth/react"
+
 
 function Sidebar() {
+  const {data: session} = useSession();
   return (
     <div className="p-2 flex flex-col h-screen">
         <div className="flex-1">
@@ -8,7 +12,12 @@ function Sidebar() {
                 <NewChat/>  
             </div>
         </div>
+{session && (
 
+  <img src={session.user?.image!} alt="" 
+  className="h-12 w-12 rounded-full cursor-pointer mb-2 mx-auto hover:opacity-50 "
+  />
+)}
     </div>
   )
 }
