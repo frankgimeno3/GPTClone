249589a4e65c49e165cc9e1b04620e7a15ebbc8e
frank gimeno <frankgimeno3@gmail.com>
2023-10-11 18:18:52 +0200

    almacena mensajes en bbdd

--- components/ChatInput.tsx
+++ components/ChatInput.tsx
@@ -1,7 +1,10 @@
 'use client'
 import { PaperAirplaneIcon } from '@heroicons/react/24/solid'
-import React, { useState } from 'react'
+import React, { FormEvent, useState } from 'react'
 import { useSession } from 'next-auth/react';
+import { addDoc, collection, serverTimestamp } from 'firebase/firestore';
+import { db } from '@/firebase';
+import toast from 'react-hot-toast';
 
 type Props = {
   chatId: string
@@ -10,9 +13,50 @@ function ChatInput({ chatId }: Props) {
   const [prompt, setPrompt] = useState("")
   const { data: session } = useSession()
 
+  const model = "text-davinci-003"
+
+  const sendMessage = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    if (!prompt) { return }
+ 
+    const input = prompt.trim()
+    setPrompt("")
+
+    const message: Message = {
+      text: input,
+      createdAt: serverTimestamp(),
+      user: {
+        _id: session?.user?.email!,
+        name: session?.user?.name!,
+        avatar: session?.user?.image! || `https://ui-avatars.com/api/?name=${session?.user?.name}`
+      }
+    }
+
+    await addDoc(
+      collection(db, 'users', session?.user?.email!, 'chats', chatId, 'messages'),
+      message
+    )
+
+    const notification = toast.loading('ChatGPT is thinking...')
+
+    await fetch('/api/askQuestion', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        prompt: input, chatId, model, session
+      }),
+    }).then(()=>{
+        toast.success("ChatGPT has responded!", {
+          id: notification
+        })
+    })
+  }
+
   return (
     <div className='bg-gray-700/50 text-gray-400 rounded-lg text-sm '>
-      <form className='p-5 space-x-5 flex-1'>
+      <form onSubmit={sendMessage} className='p-5 space-x-5 flex-1'>
         <input
           value={prompt}
           disabled={!session}
@@ -22,10 +66,10 @@ function ChatInput({ chatId }: Props) {
           className='bg-transparent focus:outline-none flex-1
          disabled:cursor-not-allowed disabled:text-gray-300'
         />
-        <button 
-        type='submit'
-        disabled= {!prompt || !session}
-        className='bg-[#11A37f] hover:opacity-50 text-white font-bold px-4 py-2 rounded
+        <button
+          type='submit'
+          disabled={!prompt || !session}
+          className='bg-[#11A37f] hover:opacity-50 text-white font-bold px-4 py-2 rounded
         disabled:bg-gray-300 disabled:cursor-not-allowed'
         >
           <PaperAirplaneIcon />
---
+++ components/ClientProvider.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Toaster } from 'react-hot-toast'
+
+function ClientProvider() {
+  return (
+    < >
+    <Toaster position='top-right'/>
+    </ >
+  )
+}
+
+export default ClientProvider
