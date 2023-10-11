bda5fc30d9488ae3afc83c5892deceac13fb40d9
frank gimeno <frankgimeno3@gmail.com>
2023-10-11 20:55:33 +0200

    me funciona sin respuesta del GPT, me dice message

---
+++ components/Message.tsx
@@ -0,0 +1,20 @@
+import { DocumentData } from "firebase-admin/firestore"
+
+
+type Props = {
+    message: DocumentData
+}
+
+export default function Message({message}:Props) {
+    const isChatGPT = message.user.name ==="ChatGPT"
+  return (
+    <div className={`py-5 text-white ${isChatGPT && "bg-[#434654]" }`}>
+        <div className="flex space-x-5 max-w-2xl mx-auto">
+            {/* <img src={message.user.avatar} alt="" className="h-8 w-8" /> */}
+            <p>
+                {message.text}
+             </p>
+        </div>
+    </div>
+  )
+}
---
+++ pages/api/getEngines.ts
@@ -0,0 +1,20 @@
+// import type { NextApiRequest, NextApiResponse } from "next";
+// import OpenAI from "openai";
+// // import openai from "../../lib/chatgpt";
+
+// type Option = {
+//     value: string;
+//     label: string;
+// }
+
+// type Data = {
+//     modelOptions: Option[]
+// }
+
+// export default async function handler(
+//     req:NextApiRequest,
+//     res:NextApiResponse) {
+//         const models = await OpenAI.listModels().then((res)=> res.data.data)
+//         const 
+    
+// }
