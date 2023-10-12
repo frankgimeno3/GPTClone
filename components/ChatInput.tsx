ae5c77a5568d28a00048723c6cadf3a10b583264
frank gimeno <frankgimeno3@gmail.com>
2023-10-12 16:43:01 +0200

    ligeras correcciones barra chat

--- components/ChatInput.tsx
+++ components/ChatInput.tsx
@@ -59,31 +59,31 @@ function ChatInput({ chatId }: Props) {
   }
 
   return (
-    <div className='bg-gray-700/50 text-gray-400 rounded-lg text-sm '>
-      <form onSubmit={sendMessage} className='p-5 space-x-5 flex-1'>
-        <input
-          value={prompt}
-          disabled={!session}
-          onChange={(e) => setPrompt(e.target.value)}
-          type='text'
-          placeholder='type your message here...'
-          className='bg-transparent focus:outline-none flex-1
-         disabled:cursor-not-allowed disabled:text-gray-300'
-        />
-        <button
-          type='submit'
-          disabled={!prompt || !session}
-          className='bg-[#11A37f] hover:opacity-50 text-white font-bold px-4 py-2 rounded
-        disabled:bg-gray-300 disabled:cursor-not-allowed'
-        >
-          <PaperAirplaneIcon />
-        </button>
+    <div className='bg-gray-700/50 text-gray-400 rounded-lg text-xs m-12 mx-24 rounded-lg'>
+      <form onSubmit={sendMessage} className='flex p-3 justify-between flex-1 '>
+          <input
+            value={prompt}
+            disabled={!session}
+            onChange={(e) => setPrompt(e.target.value)}
+            type='text'
+            placeholder='Send a message'
+            className='bg-transparent focus:outline-none flex-1 disabled:cursor-not-allowed disabled:text-gray-300  pl-2'
+            
+          />
+          <button
+            type='submit'
+            disabled={!prompt || !session}
+            className='bg-[#11A37f] hover:opacity-50 text-white font-bold px-4 py-2 rounded disabled:bg-gray-300 disabled:cursor-not-allowed'
+          >
+            <PaperAirplaneIcon className='w-4 h-4'/>
+          </button>
       </form>
       <div className='md:hidden'>
-        <ModelSelection/>
+        <ModelSelection />
       </div>
     </div>
   )
 }
+
 
 export default ChatInput
